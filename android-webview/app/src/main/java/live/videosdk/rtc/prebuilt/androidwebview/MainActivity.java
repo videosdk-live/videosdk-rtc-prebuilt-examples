73a252bf2f29cf2481285de1c52f0358cab47cc7
Rajan Surani <rajansurani14@gmail.com>
2022-10-21 14:40:52 +0530

    text update

--- android-webview/app/src/main/java/live/videosdk/rtc/prebuilt/androidwebview/MainActivity.java
+++ android-webview/app/src/main/java/live/videosdk/rtc/prebuilt/androidwebview/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private String prebuiltVersion = "0.3.21";
 
-    private String meetingId = "YOUR_MMETING_ID";
+    private String meetingId = "YOUR_MEETING_ID";
 
     private String name = "John Doe";
     private String micEnabled = "true";
